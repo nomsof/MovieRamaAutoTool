a83f62685aff80850296c855e10c3df79fe97dd3
nomsof <nomikou.sofia@gmail.com>
2016-08-23 11:14:36 +0300

    1. Changed version in pom.xml. 2. Moved function randomStringGenerator.

--- src/test/java/com/workable/movierama/tests/MovieRamaTest.java
+++ src/test/java/com/workable/movierama/tests/MovieRamaTest.java
@@ -1,6 +1,7 @@
 package com.workable.movierama.tests;
 
 import java.awt.AWTException;
+import java.util.Random;
 
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
@@ -19,61 +20,70 @@ import com.workable.movierama.session.constants.BrowserType;
 import com.workable.movierama.util.ValidationUtils;
 
 public class MovieRamaTest {
-	private static final Logger LOG = LoggerFactory.getLogger(MovieRamaTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MovieRamaTest.class);
 
-	private MainPage mainPage;
-	private NextPage nextPage;
-	private PreviousPage previousPage;
+    private MainPage mainPage;
+    private NextPage nextPage;
+    private PreviousPage previousPage;
 
-	private WebDriver driver;
-	private WebController webController;
-	private SessionDriver sessionDriver;
+    private WebDriver driver;
+    private WebController webController;
+    private SessionDriver sessionDriver;
 
+    @BeforeTest
+    public void init() {
+        sessionDriver = null;
+        try {
+            sessionDriver = new SessionDriver(BrowserType.CHROME);
+            driver = sessionDriver.getWebDriver();
+            webController = new WebController(driver);
+        } catch (Exception e) {
+            LOG.error("Error while initializing webDriver.", e);
+        }
+        mainPage = new MainPage(webController);
+        nextPage = new NextPage(webController);
+        previousPage = new PreviousPage(webController);
+        mainPage.returnToMainPage();
+    }
 
-	@BeforeTest
-	public void init() {
-		sessionDriver = null;
-		try {
-			sessionDriver = new SessionDriver(BrowserType.CHROME);
-			driver = sessionDriver.getWebDriver();
-			webController = new WebController(driver);
-		} catch (Exception e) {
-			LOG.error("Error while initializing webDriver.", e);
-		}
-		mainPage = new MainPage(webController);
-		nextPage = new NextPage(webController);
-		previousPage = new PreviousPage(webController);
-		mainPage.returnToMainPage();
-	}
+    @Test
+    public void testMovieRama() throws AWTException, InterruptedException {
+        LOG.info("-------------------------------------------------------------------------");
+        LOG.info("E2E test scenario running...");
+        mainPage.search(randomStringGenerator(5));
+        Thread.sleep(BrowserConstants.SLEEP_TIME);
+        mainPage.clearSearch();
+        Thread.sleep(BrowserConstants.SLEEP_TIME);
+        nextPage.open();
+        Thread.sleep(BrowserConstants.SLEEP_TIME);
+        previousPage.open();
+        Thread.sleep(BrowserConstants.SLEEP_TIME);
+        mainPage.selectRandomPageNumber();
+        Thread.sleep(BrowserConstants.SLEEP_TIME);
+        mainPage.selectRandomResultsSize();
+        Thread.sleep(BrowserConstants.SLEEP_TIME);
+        LOG.info("-------------------------------------------------------------------------");
+    }
 
-	@Test
-	public void testMovieRama() throws AWTException, InterruptedException {
-		LOG.info("-------------------------------------------------------------------------");
-		LOG.info("E2E test scenario running...");
-		mainPage.search(ValidationUtils.randomStringGenerator(5));
-		Thread.sleep(BrowserConstants.SLEEP_TIME);
-		mainPage.clearSearch();
-		Thread.sleep(BrowserConstants.SLEEP_TIME);
-		nextPage.open();
-		Thread.sleep(BrowserConstants.SLEEP_TIME);
-		previousPage.open();
-		Thread.sleep(BrowserConstants.SLEEP_TIME);
-		mainPage.selectRandomPageNumber();
-		Thread.sleep(BrowserConstants.SLEEP_TIME);
-		mainPage.selectRandomResultsSize();
-		Thread.sleep(BrowserConstants.SLEEP_TIME);
-		LOG.info("-------------------------------------------------------------------------");
-	}
+    @AfterTest
+    public void tearDown() {
+        try {
+            Thread.sleep(BrowserConstants.SLEEP_TIME);
+        } catch (Exception e) {
+            LOG.error("Error in tearDown", e);
+        }
+        sessionDriver.quitWebDriver();
+    }
 
-	@AfterTest
-	public void tearDown() {
-		try {
-			Thread.sleep(BrowserConstants.SLEEP_TIME);
-		} catch (Exception e) {
-			LOG.error("Error in tearDown", e);
-		}
-		sessionDriver.quitWebDriver();
-	}
-
+    private static String randomStringGenerator(int size) {
+        char[] chars = "abcdefghijklmnopqrstuvwxyz0123456789_".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 
 }
--- src/test/java/com/workable/movierama/util/ValidationUtils.java
+++ src/test/java/com/workable/movierama/util/ValidationUtils.java
@@ -2,52 +2,39 @@ package com.workable.movierama.util;
 
 import static org.testng.Assert.assertNotNull;
 
-import java.util.Random;
-
 import com.workable.movierama.model.Movie;
 
 public class ValidationUtils {
-	public static final String INTEGER_PATTERN = "\\d+";
-	public static final String YEAR_PATTERN = "\\s*\\d{4}.*";
+    public static final String INTEGER_PATTERN = "\\d+";
+    public static final String YEAR_PATTERN = "\\s*\\d{4}.*";
 
-	public static boolean isInteger(String numberStr) {
-		if (numberStr.matches(INTEGER_PATTERN)) {
-			return true;
-		}
-		return false;
-	}
+    public static boolean isInteger(String numberStr) {
+        if (numberStr.matches(INTEGER_PATTERN)) {
+            return true;
+        }
+        return false;
+    }
 
-	public static boolean isYear(String yearStr) {
-		if (yearStr.matches(ValidationUtils.YEAR_PATTERN)) {
-			return true;
-		}
-		return false;
-	}
+    public static boolean isYear(String yearStr) {
+        if (yearStr.matches(ValidationUtils.YEAR_PATTERN)) {
+            return true;
+        }
+        return false;
+    }
 
-	public static String randomStringGenerator(int size){
-		char[] chars = "abcdefghijklmnopqrstuvwxyz0123456789_".toCharArray();
-		StringBuilder sb = new StringBuilder();
-		Random random = new Random();
-		for (int i = 0; i < size; i++) {
-		    char c = chars[random.nextInt(chars.length)];
-		    sb.append(c);
-		}
-		return sb.toString();
-	}
-	
-	public static void validateMovie(Movie movie) {
-		assertNotNull(movie.getTitle(), "Title in movie " + movie + " was null.");
-		assertNotNull(movie.getDescription(), "Description in movie " + movie + " was null.");
-		assertNotNull(movie.getReview(), "Review in movie " + movie + " was null.");
-		assertNotNull(movie.getStarringActorsStr(), "Starring Actors in movie " + movie + " were null.");
-		if (!ValidationUtils.isInteger(movie.getReview())) {
-			throw new AssertionError("Reviews' field is not an integer.");
-		}
-		if (!ValidationUtils.isYear(movie.getStarringActorsStr())) {
-			throw new AssertionError("Year is not shown in element " + movie.getStarringActorsStr());
-		}
-		if (movie.getActors().size() > 5) {
-			throw new AssertionError("Starring Actors are more than 5 in element " + movie.getStarringActorsStr());
-		}
-	}
+    public static void validateMovie(Movie movie) {
+        assertNotNull(movie.getTitle(), "Title in movie " + movie + " was null.");
+        assertNotNull(movie.getDescription(), "Description in movie " + movie + " was null.");
+        assertNotNull(movie.getReview(), "Review in movie " + movie + " was null.");
+        assertNotNull(movie.getStarringActorsStr(), "Starring Actors in movie " + movie + " were null.");
+        if (!ValidationUtils.isInteger(movie.getReview())) {
+            throw new AssertionError("Reviews' field is not an integer.");
+        }
+        if (!ValidationUtils.isYear(movie.getStarringActorsStr())) {
+            throw new AssertionError("Year is not shown in element " + movie.getStarringActorsStr());
+        }
+        if (movie.getActors().size() > 5) {
+            throw new AssertionError("Starring Actors are more than 5 in element " + movie.getStarringActorsStr());
+        }
+    }
 }
